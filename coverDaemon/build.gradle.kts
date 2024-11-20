7f02f4c1f761e8cb3b537b5ef480041505da1a42
Kar Martikyan <kamartikyan@gmail.com>
2024-11-20 18:00:26 +0400

    Add an empty module for cover updates

---
+++ coverDaemon/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+  id("voice.library")
+  id("voice.compose")
+  alias(libs.plugins.anvil)
+}
+
+anvil {
+  generateDaggerFactories.set(true)
+}
+
+dependencies {
+  implementation(projects.common)
+  implementation(projects.strings)
+  implementation(projects.playback)
+  implementation(projects.data)
+
+  implementation(libs.androidxCore)
+  implementation(libs.prefs.core)
+  implementation(libs.materialDialog.core)
+  implementation(libs.androidxCore)
+  implementation(libs.material)
+  implementation(libs.seismic)
+
+  implementation(libs.dagger.core)
+
+  testImplementation(libs.prefs.inMemory)
+}
